752acb70264017b1df89e498930eee0892a06595
hoalongnatsu <hoalongnatsu@gmail.com>
2022-11-30 15:26:04 +0700

    update code

--- main.go
+++ main.go
@@ -90,8 +90,8 @@ func deploy() {
 	)
 
 	params := &elasticbeanstalk.UpdateEnvironmentInput{
-		ApplicationName: aws.String("invoicer201707071231"),
-		EnvironmentId:   aws.String("e-y8ubep55hp"),
+		ApplicationName: aws.String("invoicer"),
+		EnvironmentId:   aws.String("e-g3ixfqtddq"),
 		VersionLabel:    aws.String("invoicer-api"),
 	}
 	resp, err := svc.UpdateEnvironment(params)
